b21b1e4e5bafd5902116b092e054543a9ff56825
RakaValrizqy <myemail>
2022-02-20 19:32:24 +0700

    Pembagian Hak Akses

--- routes/api.php
+++ routes/api.php
@@ -7,19 +7,25 @@ Route::post('/register', 'UserController@register');
 Route::post('/login', 'UserController@login');
 
 Route::group(['middleware' => ['jwt.verify']], function(){
+
+    Route::group(['middleware' => ['api.superadmin']], function(){
+        Route::delete('/kelas/{id}', 'KelasController@destroy'); 
+        Route::delete('/siswa/{id}', 'SiswaController@destroy');
+    });
+
+    Route::group(['middleware' => ['api.admin']], function(){
+        Route::put('/kelas/{id}', 'KelasController@update'); 
+        Route::post('/kelas', 'KelasController@store'); 
+        Route::put('/siswa/{id}', 'SiswaController@update'); 
+        Route::post('/siswa', 'SiswaController@store'); 
+        Route::post('pinjam_buku','transaksiController@pinjamBuku');
+        Route::post('tambah_item/{id}','transaksiController@tambahItem');
+        Route::post('mengembalikan_buku','transaksiController@mengembalikanBuku'); 
+    });
+
     Route::get('/kelas', 'KelasController@show'); 
     Route::get('/kelas/{id}', 'KelasController@detail'); 
-    Route::put('/kelas/{id}', 'KelasController@update'); 
-    Route::post('/kelas', 'KelasController@store'); 
-    Route::delete('/kelas/{id}', 'KelasController@destroy'); 
-     
     Route::get('/siswa', 'SiswaController@show'); 
     Route::get('/siswa/{id}', 'SiswaController@detail'); 
-    Route::put('/siswa/{id}', 'SiswaController@update'); 
-    Route::post('/siswa', 'SiswaController@store'); 
-    Route::delete('/siswa/{id}', 'SiswaController@destroy'); 
 });
 
-Route::post('pinjam_buku','transaksiController@pinjamBuku');
-Route::post('tambah_item/{id}','transaksiController@tambahItem');
-Route::post('mengembalikan_buku','transaksiController@mengembalikanBuku');
